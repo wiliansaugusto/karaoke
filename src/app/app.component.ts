3be4fb505d93ce42c1c8e46a41ed0c5dc67f9d86
wiliansaugusto <wiliansaugusto@gmail.com>
2024-06-28 13:28:52 -0300

    Alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+interface DadosMusicais {
+  id: number;
+  artista: string;
+  musica: string;
+  link: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
-export class AppComponent {
-  title = 'karaoke';
+
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['artista', 'musica', 'link'];
+  dataSource!: MatTableDataSource<any>;
+  musicaNacional!: DadosMusicais[];
+  @ViewChild('paginatorNacional') paginator!: MatPaginator;
+  @ViewChild('sort') sort!: MatSort;
+
+  dataSourceInternacional!: MatTableDataSource<any>;
+  musicaInternacional!: DadosMusicais[];
+  @ViewChild('paginatorInternacional') paginatorInternacional!: MatPaginator;
+  @ViewChild('sortInternacional') sortInternacional!: MatSort;
+  url!: SafeResourceUrl;
+  artista: any;
+  musica: any;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer,iconRegistry: MatIconRegistry, ) {
+    iconRegistry.addSvgIcon(
+      'mic',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/microphone-with-cable-svgrepo-com.svg')
+    );
+
+   }
+
+
+  ngOnInit(): void {
+    this.http.get<any>("assets/json/musicaNacional.json").subscribe((resp: any) => {
+      this.musicaNacional = resp.musicaNacional
+      this.musicaNacional.sort((a, b) => a.artista.toLowerCase().localeCompare(b.artista))
+
+      this.dataSource = new MatTableDataSource(this.musicaNacional);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.randomMusicaNacional();
+
+
+    })
+    this.http.get<any>("assets/json/musicaInternacional.json").subscribe((resp: any) => {
+      this.musicaInternacional = resp.musicaInternacional;
+      this.musicaInternacional.sort((a, b) => a.artista.toLowerCase().localeCompare(b.artista))
+
+      this.dataSourceInternacional = new MatTableDataSource(this.musicaInternacional);
+      this.dataSourceInternacional.paginator = this.paginatorInternacional;
+      this.dataSourceInternacional.sort = this.sortInternacional;
+
+    })
+  }
+  aleatorioTotal() {
+    const randomIndex = Math.floor(Math.random() * 2);
+    console.warn(randomIndex);
+    if (randomIndex == 1) {
+      this.randomMusicaInternacional()
+    } else {
+      this.randomMusicaNacional();
+    }
+
+
+  }
+  randomMusicaNacional() {
+    const randomIndex = Math.floor(Math.random() * this.musicaNacional.length);    
+    this.tocar(this.musicaNacional[randomIndex])
+  }
+  randomMusicaInternacional() {
+    const randomIndex = Math.floor(Math.random() * this.musicaInternacional.length);
+    this.tocar(this.musicaInternacional[randomIndex])
+
+  }
+  getSafeUrl(url: any): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  applyFilterInternacional(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceInternacional.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSourceInternacional.paginator) {
+      this.dataSourceInternacional.paginator.firstPage();
+    }
+  }
+
+  tocar(dados: any) {
+    this.artista = dados.artista
+    this.musica = dados.musica
+    const tocar = this.getSafeUrl(`${dados.link}?autoplay=1`);
+
+    this.url = tocar;
+
+    const element = document.getElementById('player');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+    
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+ import {MatTableModule} from '@angular/material/table'; 
+import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatTabsModule} from '@angular/material/tabs'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule,
+    MatTableModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
